46ff9ad5b8509c67855981c47b1b8d31b8573d34
DhanishMecha <dhanishp@ext.mechasystems.com>
2026-05-06 17:43:19 +0530

    feat: add project assets and dependencies

---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,3 @@
+class AppRoutes {
+  static const noteEditor = '/note-editor';
+}
---
+++ lib/core/utils/icons.dart
@@ -0,0 +1,19 @@
+class NotesIcon {
+  static const String editIcon = "assets/icons/edit.png";
+  static const String gridIcon = "assets/icons/grid.png";
+  static const String listIcon = "assets/icons/list.png";
+  static const String createIcon = "assets/icons/create.png";
+  static const String searchIcon = "assets/icons/search.png";
+  static const String backIcon = "assets/icons/back.png";
+  static const String boldIcon = "assets/icons/bold.png";
+  static const String italicIcon = "assets/icons/italic.png";
+  static const String underlineIcon = "assets/icons/underline.png";
+  static const String undoIcon = "assets/icons/undo.png";
+  static const String redoIcon = "assets/icons/redo.png";
+  static const String inlinecodeIcon = "assets/icons/inlinecode.png";
+  static const String h1Icon = "assets/icons/h1.png";
+  static const String h2Icon = "assets/icons/h2.png";
+  static const String textstyleIcon = "assets/icons/textstyle.png";
+  static const String menuIcon = "assets/icons/menu.png";
+  static const String paragraphIcon = "assets/icons/paragraph.png";
+}
